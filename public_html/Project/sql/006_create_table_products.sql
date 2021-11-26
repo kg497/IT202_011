3c1c06233e962c1f1f5821682c8a9ddc076d22b0
kg497 <kg497@njit.edu>
2021-11-26 12:44:15 -0500

    added ways for admin to add and update shop data

---
+++ public_html/Project/sql/006_create_table_products.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE IF NOT EXISTS Products(
+    id int AUTO_INCREMENT PRIMARY  KEY,
+    name varchar(30) UNIQUE, -- alternatively you'd have a SKU that's unique
+    description text,
+    category varchar(30) default 'uncategorized',
+    stock int DEFAULT  0,
+    unit_price int DEFAULT  99999,
+    /*image text, -- this col type can't have a default value*/
+    visibility tinyint(1) default false,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+)
