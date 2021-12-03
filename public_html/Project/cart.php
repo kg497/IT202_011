1086c95f35905910dd067cc718a6eb7614cac32c
kg497 <kg497@njit.edu>
2021-12-02 22:33:49 -0500

    minor change

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -5,6 +5,8 @@ is_logged_in(true);
 <?php
 // go to cart and go to speccific user, get product_id - go to Products table to get name, get quantity, and unit_price
 // go to Products table to get the name of the item,
+$bad =0;
+$total=0;
 $res = [];
 $columns = get_columns("Cart");
 //echo "<pre>" . var_export($columns, true) . "</pre>";
@@ -12,7 +14,7 @@ $ignore = ["id", "modified", "created"];
 $result=[];
 $db = getDB();
 $user_id = get_user_id();
-$stmt = $db->prepare("SELECT user_id, product_id, desired_quantity, unit_price from Cart WHERE user_id = :user_id ORDER BY modified");
+$stmt = $db->prepare("SELECT Cart.user_id, Cart.product_id, Cart.desired_quantity, Cart.unit_price, Products.name FROM Cart INNER JOIN Products ON Cart.product_id = Products.id WHERE Cart.user_id = :user_id");
 //$stmt2 = $db->prepare("SELECT id, name from Products WHERE user_id = :user_id");
 if (isset($_POST["deletion"])) {
     $p = se($_POST, "product_id", -1, false);
@@ -32,11 +34,32 @@ if (isset($_POST["clear"])) {
         flash("<pre>" . var_export($e, true) . "</pre>");
     }
 }
+if (isset($_POST["desired_quantity"])) {
+    $p= se($_POST, "product_id", -1, false);
+    $q= se($_POST, "desired_quantity", 0, false);
+    $stmt4 = $db->prepare("UPDATE Cart SET desired_quantity = $q WHERE user_id = :u_id AND product_id= :product_id");
+    if($q != 0){
+        try {
+            $stmt4->execute([":u_id" => $user_id, ":product_id"=> $p]);
+        }catch (PDOException $e) {
+            flash("<pre>" . var_export($e, true) . "</pre>");
+        }
+    }
+    else{
+        $user_id =get_user_id();
+        $stmt5 = $db->prepare("DELETE from Cart WHERE product_id = :product_id AND user_id = :u_id");
+        try {
+            $stmt5->execute([":u_id" => $user_id, ":product_id" => $p]);
+        }catch (PDOException $e) {
+            flash("<pre>" . var_export($e, true) . "</pre>");
+        }
+    }
+    
+}
 try {
     $stmt->execute([":user_id" => $user_id]);
     $r = $stmt->fetchALL(PDO::FETCH_ASSOC);
     if ($r) {
-        
         $result = $r;
     }
     
@@ -57,48 +80,56 @@ function mapColumn($col)
 ?>
 <div class="container-fluid">
     <h1>My Cart</h1>
+    <form method="POST" class="row row-cols-lg-auto g-3 align-items-center">
+        <div class="mb-4">
+            <input type = "hidden" name = "product_id" value = "<?php se($item, "product_id");?>" />
+            <input class="btn btn-primary" name = "clear" type="submit" value="Clear Cart" />
+        </div>              
+    </form>
     <?php foreach ($result as $item) : ?>
 
         <?php 
         $product= $item["product_id"];
         ?>
-        <form method="POST" class="row row-cols-lg-auto g-3 align-items-center">
-            <div class="mb-4">
-                <input type = "hidden" name = "product_id" value = "<?php se($item, "product_id");?>" />
-                <input class="btn btn-primary" name = "clear" type="submit" value="Clear Cart" />
-            </div>              
-        </form>
+        <?php $total = $total + ((int)(100*$item["unit_price"])* $item["desired_quantity"])/100;
+        $product_tot =((int)(100*$item["unit_price"])* $item["desired_quantity"])/100;
+        ?>
+            
             <div class="col">
                 <div class="card">
                     <div class="card-header">
-                        
+                        <h5 class="card-title"><?php se($item, "name"); ?></h5>
                     </div>
                     <div class="card-body">
-                    <p class="card-text">Quantity <?php se($item, "desired_quantity"); ?></p>
+                    <p class="card-text">Unit Price: $ <?php se($item, "unit_price");?></p>
+                    <p class="card-text">Total Cost: $ <?php se($product_tot);?></p>
                     
                     <form method="POST" class="row row-cols-lg-auto g-3 align-items-center">
                         <div class="mb-4">
-                            <label class="form-label" for="quantity">Update Quantity </label>
-                            <input class="form-control" id="quantity" name="quantity" />
+                            <input type = "hidden" name = "product_id" value = "<?php se($item, "product_id");?>"/>
+                            <label class="form-label" for="quantity">Quantity <?php se($item, "desired_quantity"); ?> </label>
+                            <input class="form-control" id="desired_quantity" name="desired_quantity" value= "<?php se($item, "desired_quantity");?>" />
                         </div>
                         <input type="submit" class="btn btn-primary" value="Update" />
                     </form>
                     <a href="details.php?id=<?php se($item, "product_id");?>">Details</a>   
                     </div>
                     <div class="card-footer">
-                        Cost: $ <?php se($item, "unit_price"); ?>
-                        <input type=button onClick="location.href='add_cart.php?id=<?php se($item, "id");?>unit_price=<?php se($item, "unit_price");?>'" class = "btn btn-primary" value='Purchase'>
+                        
                         <form method="POST" class="row row-cols-lg-auto g-3 align-items-center">
                         <div class="mb-4">
                             <input type = "hidden" name = "product_id" value = "<?php se($item, "product_id");?>" />
                             <input class="btn btn-primary" name = "deletion" type="submit" value="Delete Item" />
                         </div>
-                        
-                    </form>
+                        </form>
+                        <input type=button onClick="location.href='add_cart.php?id=<?php se($item, "id");?>unit_price=<?php se($item, "unit_price");?>'" class = "btn btn-primary" value='Purchase'>
                     </div>
                 </div>
             </div>
         <?php endforeach; ?>
+    <div>
+        <p class="card-text" ><font size = 4>Total Cart Cost: $ <?php se($total);?></font></p>
+    </div>
 </div>
 
 <?php
